ffc614b2c4fad98bfcdd963ced580c886779d650
richyliu <richy.liu.2002@gmail.com>
2018-11-10 14:14:46 -0800

    add typescript react template

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # templates
 Common templates
+
+# Typescript React
+Typescript React starter template comes with React, Typescript, Material UI, React Router, and the autobind decorator (to prevent reduant `this`)
+
+## Direct copy
+Copy the folder
+```shell
+cp -nr ./templates/typscript_react/ app/
+cd app
+npm i
+```
+
+## Create the app manually
+This guide assumes installing to the `./app` directory. If this is not the case, change all `app` to the specified directory
+
+First, install create-react-app
+```shell
+npm install -g create-react-app
+```
+
+Create the project. Make sure to change the `app` parameter to the name of your app
+```shell
+create-react-app app --scripts-version=react-scripts-ts
+```
+
+Install dependencies
+```shell
+cd ./app
+npm i @material-ui/core @material-ui/icons typeface-roboto @types/react-router-dom autobind-decorator react-router-dom
+```
+
+Copy the configurations
+```shell
+cp ./templates/typescript_react/tslint.json ./app/
+cp ./templates/typescript_react/tsconfig.json ./app/
+cp ./templates/typescript_react/.editorconfig ./app/
+```
+
+Add the following lines to `./app/.gitignore`
+```
+.idea/
+.vscode/
+```
---
+++ typescript_react/src/App.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import StoreIcon from '@material-ui/icons/Store';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+import boundMethod from 'autobind-decorator';
+
+import './App.css';
+import logo from './logo.svg';
+
+const HomeRedir = () => <Redirect to="/home" />;
+const Home = () => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.tsx</code> and save to reload.
+  </p>
+);
+const Store = () => <p>Store page</p>;
+const Settings = () => <p>Settings page</p>;
+
+interface AppState {
+  value: string;
+}
+
+class App extends React.Component<any, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '/home'
+    };
+  }
+
+  public componentDidMount() {
+    this.setState({
+      value: this.props.history.location.pathname
+    });
+  }
+
+  @boundMethod
+  public handleChange(_, value: string) {
+    this.props.history.push(value);
+    this.setState({ value });
+  }
+
+  public render() {
+    const { value } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <Switch>
+          <Route exact path="/" component={HomeRedir} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/store" component={Store} />
+          <Route exact path="/settings" component={Settings} />
+        </Switch>
+
+        <BottomNavigation value={value} onChange={this.handleChange} showLabels>
+          <BottomNavigationAction
+            label="Home"
+            value="/home"
+            icon={<HomeIcon />}
+          />
+          <BottomNavigationAction
+            label="Store"
+            value="/store"
+            icon={<StoreIcon />}
+          />
+          <BottomNavigationAction
+            label="Settings"
+            value="/settings"
+            icon={<SettingsIcon />}
+          />
+        </BottomNavigation>
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
